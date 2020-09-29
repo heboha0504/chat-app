c601bd5848eaf812b38ceaeaf33912879b44fb53
heboha0504 <hebonobank@gmail.com>
2020-09-29 16:05:23 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, persence: true
+  validates :content, persence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.image.attached?
+  end  
 end
